8e7e52c0371fa78a722a1ae022a8f18c45d05d4d
Lucas Tomaz de Moura <69602320+lucas-tomaz@users.noreply.github.com>
2024-10-07 13:28:31 -0300

    Update simulacao_01.m
    versão final do código de simulação feito no OCTAVE

--- simulacao_01.m
+++ simulacao_01.m
@@ -1,10 +1,27 @@
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE
+# DEPARTAMENTO DE COMPUTAÇÃO E AUTOMAÇÃO
+# PROCESSAMENTO DIGITAL DE SINAIS
+# ALUNO: Lucas Tomaz de Moura
+# #1 Simulação computacional: Convolução da soma.
+
+#declaração das variáveis dos caminhos dos arquivos
 caminho = 'Church_Schellingwoude.wav'
-[Y, Fs] = audioread(caminho)
-Y = Y(:,1)
-%sound(Y, Fs) reprodução do audio
-t = (0:length(Y)-1) / Fs;
-figure;
-plot(t, Y);
-xlabel('Tempo (s)');
-ylabel('Amplitude');
-title('Forma de Onda do Áudio');
+caminho2 = 'Teste_de_voz.wav'
+
+#criação dos vetores dos dois arquivos de áudios e determinação das frequências de amostragem.
+[Y_impulso, Fs_impulso] = audioread(caminho)
+[Y_voz, Fs_voz] = audioread(caminho2)
+
+#delimitação apara apenas usar a primeira coluna do vetor do impulso, já que inicialmente ele é estéreo.
+Y_impulso = Y_impulso(:,1)
+
+#reprodução dos sons(usado nos testes para validar a transformação em vetores
+#sound(Y_impulso, Fs_impulso)
+#sound(Y_voz, Fs_voz)
+
+#convolução entre a voz e o impulso.
+resultado_convolucao = conv(Y_voz, Y_impulso)
+
+#criação e escrita do arquivo de áudio resultado da convolução.
+arquivo = 'convolucao.wav'
+audiowrite(arquivo, resultado_convolucao, Fs_voz)
