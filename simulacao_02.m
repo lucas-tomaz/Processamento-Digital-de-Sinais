07caa6b28099cab146c1da4bb14e60f1ef45928f
Lucas Tomaz de Moura <69602320+lucas-tomaz@users.noreply.github.com>
2024-11-27 13:16:56 -0300

    Update simulacao_02.m

--- simulacao_02.m
+++ simulacao_02.m
@@ -1,3 +1,12 @@
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE
+# DEPARTAMENTO DE COMPUTAÇÃO E AUTOMAÇÃO
+# PROCESSAMENTO DIGITAL DE SINAIS
+# ALUNO: Lucas Tomaz de Moura
+# #2 Simulação computacional: Resposta ao impulso e da resposta em frequência do filtro FIR desenvolvido na parte 1.
+#Parâmetros Escolhidos: N° 7.
+  #freq_ruido_1 = 5700 #5.7 KHz
+  #freq_ruido_2 = 6000 #6 KHz
+
 h_valores = [];  % Inicialização da lista h_valores
 
 sinal_valores = [];
@@ -5,8 +14,8 @@ sinal_valores = [];
 a_1 = 0.5
 a_2 = 0.5
 
-freq_ruido_1 = 5700 #5.7 KHz
-freq_ruido_2 = 6000 #6 KHz
+#freq_ruido_1 = 5700 #5.7 KHz
+#freq_ruido_2 = 6000 #6 KHz
 Ts = 1/44100 #período de amostragem
 
 for n = 0:36
@@ -16,16 +25,10 @@ for n = 0:36
         freq_1 = (sin(41 / 200 * pi * (n - 18))) / (pi * (n - 18));
         freq_2 = (sin(63 / 200 * pi * (n - 18))) / (pi * (n - 18));
         h = freq_1 - freq_2;
-        ruido = a_1*cos(2* pi*freq_ruido_1*n*Ts) + a_2*cos(2* pi*freq_ruido_2*n*Ts)
-        
-        sinal_total  = h + ruido
     end
-    
+
     h_valores = [h_valores, h];  % Adiciona o valor de h na lista h_valores
-    sinal_valores = [sinal_valores, sinal_total];
-    
+
   end
 
 plotspec(h_valores, Ts)
-
-plotspec(sinal_valores, Ts)
